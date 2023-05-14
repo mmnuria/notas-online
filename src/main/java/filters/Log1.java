83f9e54291c28b7ff2760391f83d21c951908de1
mmnuria <nuriamanzanomata@gmail.com>
2023-05-14 14:15:57 +0200

    Log1 working
    - Change the log path.
    - Check if the log path exists and create if not exists.

--- src/main/java/filters/Log1.java
+++ src/main/java/filters/Log1.java
@@ -1,6 +1,7 @@
 package filters;
 
 import java.io.FileWriter;
+import helpers.FileSystem;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
@@ -17,18 +18,27 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Log1")
 public class Log1 extends HttpServlet {
 
-    private static final String DEFAULT_LOG_FILE_PATH = "/var/log/notas-online/access.log";
+    private static final String DEFAULT_LOG_FILE_PATH = "/home/user/notas-online";
+    private static final String DEFAULT_LOG_FILE_NAME = "access.log";
 
     private PrintWriter logWriter;
 
+    private FileSystem fileSystem = new FileSystem();
+    
     public void init() throws ServletException {
         // Initialization code goes here
 
         // Open log file for writing
         try {
-            logWriter = new PrintWriter(new FileWriter(DEFAULT_LOG_FILE_PATH, true));
+        	if(!fileSystem.exists(DEFAULT_LOG_FILE_PATH)) {
+        		fileSystem.createDirectory(DEFAULT_LOG_FILE_PATH);
+        	}
+        	fileSystem.createFile(DEFAULT_LOG_FILE_PATH, DEFAULT_LOG_FILE_NAME);
+            logWriter = new PrintWriter(new FileWriter(DEFAULT_LOG_FILE_PATH + "/" + DEFAULT_LOG_FILE_NAME, true));
         } catch (IOException e) {
             throw new ServletException("Error opening log file", e);
+        } catch(Exception e) {
+        	throw new ServletException("Error creating directory", e);
         }
     }
 
@@ -39,13 +49,14 @@ public class Log1 extends HttpServlet {
         String formData = request.getQueryString();
         String clientInfo = request.getRemoteUser() + " " + request.getRemoteAddr();
         String currentDate = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-        String pathInfo = request.getPathInfo();
-        // String uri = request.getRequestURI();
+        String servletName = request.getServletPath();
+        String uri = request.getRequestURI();
         String method = request.getMethod();
 
         // Log the entry
-        String logEntry = currentDate + " " + clientInfo + " " + pathInfo + " " + method;
+        String logEntry = currentDate + " " + clientInfo + " " + servletName + " " + method;
         System.out.println(logEntry);
+        
         logWriter.println(logEntry);
         logWriter.flush();
     }
